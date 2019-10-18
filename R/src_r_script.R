5bd401fd01444a16cf6e294075d5ea5a1d59e08b
Jay Lee <jaylee@att.com>
2019-10-18 14:46:39 -0400

    play w/ theme and highlight

--- R/src_r_script.R
+++ R/src_r_script.R
@@ -13,8 +13,9 @@ print(p)
 p <- ncaa %>% count(CHAMPION) %>%
          arrange(desc(n)) %>% 
          filter(n >= 3) %>%
-         ggplot(aes(x = reorder(CHAMPION, -n), y = n)) +
+         ggplot(aes(x = reorder(CHAMPION, n), y = n)) +
          geom_bar(stat = "identity") +
+         coord_flip() +
          labs(title = "NCAA Men's Basketball Champions (3+ Rings Club!)", x = "School") +
          theme_bw()
 
--- py/src_python_script.py
+++ py/src_python_script.py
@@ -12,6 +12,6 @@ print(p)
 
 ncaa_grp = ncaa.groupby(["CHAMPION"]).size().reset_index(name = 'n').sort_values(by = "n", ascending = False)
 p = ncaa_grp[ncaa_grp.n >= 3]
-p.plot(kind = 'bar', x = 'CHAMPION', y = 'n', title = "NCAA Men's Basketball Champions (3+ Rings Club!)")
+p.plot(kind = 'barh', x = 'CHAMPION', y = 'n', title = "NCAA Men's Basketball Champions (3+ Rings Club!)", figsize = (12,8)).invert_yaxis()
 plt.show()
 
